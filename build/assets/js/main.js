059b6ea3d1b5734c64c8d24f31a564047d31d7d5
KEKACHY <kekachy@mail.ru>
2026-01-22 17:43:15 +0200

    Final

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,1 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => { console.log("Сайт загружен!"); });
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Сайт загружен!");
+});
